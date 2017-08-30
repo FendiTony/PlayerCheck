fe2d48b11d90130b1188aa35ca4f26724c90b93c
FendiTony <gesudabetlemme@gmail.com>
2017-08-30 17:34:05 +0200

    PlayerCheck1.0
    /check <player>
    Freeze <player>, send messages to him,
    plus ban on disconnect!
    BETA 1.0

---
+++ src/me/bukkit/vipera37/PlayerCheck.java
@@ -0,0 +1,59 @@
+package me.bukkit.vipera37;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class PlayerCheck extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getLogger().info(ChatColor.LIGHT_PURPLE + "Plugin has been enabled correctly!");
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		if (cmd.getName().equalsIgnoreCase("check") && sender instanceof Player) {
+
+			Player player = (Player) sender;
+
+			int lenght = args.length;
+			if (lenght == 1) {
+				boolean playerFound = false;
+				for (Player playerToCheck : Bukkit.getServer().getOnlinePlayers()) {
+					if (playerToCheck.getName().equalsIgnoreCase(args[0])) {
+						playerToCheck.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000, 9));
+						playerToCheck.sendMessage(ChatColor.RED + "" + ChatColor.UNDERLINE
+								+ "Stop RIGHT NOW! Admins will check if you're using hacks.");
+						playerToCheck.sendMessage(ChatColor.DARK_RED + "You're going to be checked by: "
+								+ ChatColor.AQUA + "" + player.getName() + ChatColor.RESET + "" + ChatColor.RED + ""
+								+ "If you disconnect you will get banned instantly!");
+						playerFound = true;
+						break;
+
+					}
+				}
+				if (playerFound == false) {
+					player.sendMessage(ChatColor.RED + args[0] + "Is not online");
+					player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_PLACE, 1.0f, 1.0f);
+				}
+			} else
+				player.sendMessage(ChatColor.RED + player.getName() + "You must write a correct command!");
+			player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_PLACE, 1.0f, 1.0f);
+			return true;
+		}
+
+		return false;
+	}
+
+}
