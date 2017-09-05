3f70c43120148743ee1511a4a0b99dbe2fef113c
FendiTony <gesudabetlemme@gmail.com>
2017-09-05 20:05:17 +0200

    more /cinfo info
    more messages on /cinfo

--- src/me/bukkit/vipera37/commands/Info.java
+++ src/me/bukkit/vipera37/commands/Info.java
@@ -10,7 +10,9 @@ public class Info implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("cinfo") && sender instanceof Player) {
 			Player player = (Player) sender;
-			player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Plugin written by: FendiTony");
+			player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Plugin written by: " + ChatColor.RED + " FendiTony777");
+			player.sendMessage(ChatColor.GOLD + "PlayerCheck version: " + ChatColor.RED + " 2.1.3");
+			player.sendMessage(ChatColor.GOLD + "New features will come soon!");
 			return true;
 		}
 		return false;
